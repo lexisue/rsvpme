f131aea439c29849cc304bf9dae1ccbe5ff2514f
Alex Sue <buttermilktoast@gmail.com>
2015-02-13 22:34:07 -1000

    Initial write up of getGuestList

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,3 +1,20 @@
+var _ = require('underscore.js');
+
+Parse.Cloud.define("getGuestList", function(request, response) {
+
+	var query = new Parse.Query(Parse.User);
+
+	query.find().then(function(results) {
+
+		var toReturn = {};
+		toReturn.code = 200;
+		toReturn.items = results;
+		response.success(toReturn);
+	}), function (error) {
+		response.error('Error querying users');
+	}
+});
+
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
